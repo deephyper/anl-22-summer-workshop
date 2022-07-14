773bab3031051f199b87849f358fe8169f1a17ea
Kyle Gerard Felker <felker@anl.gov>
2022-07-14 11:54:15 -0500

    Add Summit install script

---
+++ scripts/OLCF-Summit/dh_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+module load open-ce/1.5.0-py38-0
+conda create -p dh --clone open-ce-1.5.0-py38-0 -y
+
+# module load open-ce/1.5.2-py39-0
+# conda create -p dh --clone open-ce-1.5.2-py39-0 -y
+
+conda activate dh
+
+module load gcc
+
+MPICC=mpicc pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
+
+# git clone https://github.com/mpi4py/mpi4py.git
+# cd mpi4py/
+# MPICC=mpicc python setup.py install
+
+pip install deephyper==0.4.2
