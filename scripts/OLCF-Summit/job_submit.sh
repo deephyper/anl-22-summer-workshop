023fbcee13779e953cb4d5cd58d4a0b668b172f6
Kyle Gerard Felker <felker@anl.gov>
2022-07-15 02:33:28 -0500

    Update job_submit.sh

--- scripts/OLCF-Summit/job_submit.sh
+++ scripts/OLCF-Summit/job_submit.sh
@@ -17,7 +17,7 @@ RANKS_PER_NODE=6
 
 # Initialization of environment
 source $INIT_SCRIPT
-
+cd anl-22-summer-workshop/
 jsrun -n $(( $NUM_NODES * $RANKS_PER_NODE )) -r6 -g1 -a1 -c1 python evaluator_mpi.py
 
 
