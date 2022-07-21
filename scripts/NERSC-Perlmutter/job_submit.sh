94365d58f2189ef8c86c4767f01f92879b9f9c6a
Nesar Ramachandra <nayaknesar@gmail.com>
2022-07-21 02:27:57 -0700

    Update job_submit.sh
    updated srun command to specify gpu use.

--- scripts/NERSC-Perlmutter/job_submit.sh
+++ scripts/NERSC-Perlmutter/job_submit.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-#SBATCH -A dasrepo_g
+#SBATCH -A <allocation name>
 #SBATCH --job-name=dh_cbo_lstm
 #SBATCH -C gpu
 #SBATCH -q regular
 #SBATCH -t 1:00:00
-#SBATCH --nodes=128
-#SBATCH --gres=gpu:4
-# #SBATCH --gpus-per-task=2
+#SBATCH --nodes=2
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-task=1
+#SBATCH --gpus=8
 
 
 # User Configuration
 INIT_SCRIPT=$PWD/load_modules.sh
 
-SLURM_JOBSIZE=128
+SLURM_JOBSIZE=2
 RANKS_PER_NODE=4
 
 # Initialization of environment
 source $INIT_SCRIPT
 
-srun -N $SLURM_JOBSIZE -n $(( $SLURM_JOBSIZE * $RANKS_PER_NODE )) python evaluator_mpi.py
-
+# Slurm run
+srun -n $(( $SLURM_JOBSIZE * $RANKS_PER_NODE )) -G $(( $SLURM_JOBSIZE * $RANKS_PER_NODE )) -N $SLURM_JOBSIZE --gpus-per-task 1 --ntasks-per-node 4 python evaluator_mpi.py
 
 echo "Complete"
