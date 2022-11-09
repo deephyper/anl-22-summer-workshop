dcfec44af6c03af3169112bb3519990ada7dcfdc
Nesar Ramachandra <nayaknesar@gmail.com>
2022-11-09 16:07:16 -0600

    Update load_modules.sh

--- scripts/NERSC-Perlmutter/load_modules.sh
+++ scripts/NERSC-Perlmutter/load_modules.sh
@@ -4,4 +4,5 @@
 module load PrgEnv-nvidia cudatoolkit python
 module load cudnn/8.2.0
 source /global/common/software/nersc/pm-2022q3/sw/python/3.9-anaconda-2021.11/etc/profile.d/conda.sh
+export MPI4PY_RC_RECV_MPROBE='False'
 conda activate dhenv
